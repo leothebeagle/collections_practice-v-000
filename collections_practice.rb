8bee6eb75744acc970bbd952c14ada2ae13c481e
Aziz Kosber <a.kosber@gmail.com>
2018-05-27 20:47:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
-def sort_array_asc(array)
+def sort_array_asc(array)#***note that I didnt just start off with this. I actually built out the methods to understand whats going on. this is the final version for elegance and readability.
   array.sort
 end
+
+
