48e1015077e4388be9fe7f4100574f687202f978
Aziz Kosber <a.kosber@gmail.com>
2018-05-27 21:21:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def sort_array_asc(array)#***note that I didnt just start off with this. I actually built out the methods to understand whats going on. this is the final version for elegance and readability.
   array.sort
 end
@@ -14,7 +16,7 @@ def sort_array_char_count(array)
   end
 end
 
-def swap_elements(array)
+def swap_elements(array) #this method needs to be further abstracted
   x = array[1]
   y = array[2]
   swapped_array = []
@@ -23,3 +25,14 @@ def swap_elements(array)
   swapped_array << x
   swapped_array
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  x = array[index]
+  y = array[destination_index]
+  swapped_array = []
+  swapped_array << array[0]
+  swapped_array << y
+  swapped_array << x
+  swapped_array
+end
+binding.pry
